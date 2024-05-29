533fe837d30cfc8ff3e7a7332a6519a145731353
realkoder <alex8970@stud.kea.dk>
2024-05-29 11:39:18 +0200

    Mde use of path for deployment with vercel

--- server/utils/loadEmailTemplates.js
+++ server/utils/loadEmailTemplates.js
@@ -1,14 +1,19 @@
 import fs from 'fs';
+import path from 'path';
+
+const __dirname = path.resolve();
 
 export const signupEmailTemplate = fs.readFileSync(
-  './public/email_templates/signup/index.html',
+  path.join(__dirname, 'public/email_templates/signup/index.html'),
 );
 
 export const requestPasswordResetEmailTemplate = fs.readFileSync(
-  './public/email_templates/request_password/index.html',
+  path.join(__dirname, 'public/email_templates/request_password/index.html'),
 );
 
-export const passordUpdatedEmailTemplate = fs.readFileSync('./public/email_templates/password_updated/index.html');
+export const passwordUpdatedEmailTemplate = fs.readFileSync(
+  path.join(__dirname, 'public/email_templates/password_updated/index.html'),
+);
 
 export function renderEmailTemplate(emailTemplate, config = {}) {
   let renderedEmailTemplate = emailTemplate
