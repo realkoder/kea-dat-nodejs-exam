78a6aa33e832b776116f1a2eadd07e604dfebede
realkoder <alex8970@stud.kea.dk>
2024-06-10 12:53:01 +0200

    Fixed url to deploy

--- client/src/stores/generalStore.js
+++ client/src/stores/generalStore.js
@@ -1,5 +1,5 @@
 import { readable } from 'svelte/store';
 
-export const BASE_URL = readable('http://localhost:8080');
+// export const BASE_URL = readable('http://localhost:8080');
 // export const BASE_URL = readable('http://192.46.234.186');
-//export const BASE_URL = readable('https://kea-exam.intellioptima.com');
+export const BASE_URL = readable('https://kea-exam.intellioptima.com');
